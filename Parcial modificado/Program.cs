7584525efdbdd2298813c50d80c743ca78b72623
cindy <MINEDUCYT@CINDY-VALENCIA>
2022-09-13 16:00:17 -0600

    Agregar archivos de proyecto.

---
+++ Parcial modificado/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+
+namespace Parcial_modificado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Roses;
+            do
+            {
+                int numberStudents;
+                ArrayList registro = new ArrayList();
+                string nombre;
+                int edad;
+                double nota1;
+                double nota2;
+                double parcial;
+                double promedio;
+
+                int contador = 0;
+
+                Console.WriteLine("Ingrese el numero de estudiantes a evaluar: ");
+                numberStudents = int.Parse(Console.ReadLine());
+                Console.WriteLine("");
+                while (contador < numberStudents)
+                {
+                    Console.Write("Ingrese el nombre del estudiante: ");
+                    nombre = Console.ReadLine();
+                    Console.Write("Ingrese la edad del estudiante: ");
+                    edad = int.Parse(Console.ReadLine());
+                    Console.Write("Ingrese la nota de Laboratorio 1: ");
+                    nota1 = int.Parse(Console.ReadLine());
+                    Console.Write("Ingrese la nota de Laboratorio 2: ");
+                    nota2 = int.Parse(Console.ReadLine());
+                    Console.Write("Ingrese la nota parcial: ");
+                    parcial = int.Parse(Console.ReadLine());
+                    Console.WriteLine("");
+
+                    promedio = (nota1 * .3) + (nota2 * .3) + (parcial * .4);
+
+                    Console.WriteLine("El promedio total del alumno es: {0}", promedio);
+
+                    if (promedio >= 6)
+                    {
+                        Console.WriteLine("ALUMNO APROBADO");
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("ALUMNO REPROBADO");
+                    }
+
+
+
+
+                    Students students = new Students(nombre, edad, nota1, nota2, parcial, promedio );
+                    registro.Add(students);
+
+                    contador++;
+                }
+
+
+                Console.WriteLine(" --- RESULTADOS --- ");
+                foreach (Students st in registro)
+                {
+                    Console.WriteLine(st.getData());
+                }
+
+                Console.WriteLine("¿Quiere finalizar el proceso? Escriba el numero 5");
+                Roses = Convert.ToInt32(Console.ReadLine());
+
+            } while (Roses != 5);
+            Console.WriteLine("Finalizo el proceso");
+        }
+
+
+
+    }
+
+
+
+
+    
+    
+    
+}
---
+++ Parcial modificado/Students.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Parcial_modificado
+{
+    class Students
+    {
+        private string _nombre;
+        private int _edad;
+        private double _nota1;
+        private double _nota2;
+        private double _parcial;
+        private double _promedio;
+
+        public Students(string nombre, int edad, double nota1, double nota2, double parcial, double promedio)
+        {
+            this._nombre = nombre;
+            this._edad = edad;
+            this._nota1 = nota1;
+            this._nota2 = nota2;
+            this._parcial = parcial;
+            this._promedio = promedio;
+        }
+
+        public string getData()
+        {
+            return "Nombre: " + _nombre + " Edad: " + _edad + " Nota1:" + _nota1 + " Nota2: " + _nota2 + " Parcial: " + Parcial + " Promedio: " + Promedio;
+        }
+    }
+}
